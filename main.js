2cefd6845e5adbe602203b246930a9f60e66c46f
vihoman <124776339+vihoman@users.noreply.github.com>
2024-05-14 22:14:59 +0200

    changes to js/html
    - order button works with class for orders
    - class gets meat veggies and doneness and randomizes them
    - order list shows in customers chat bubble & (almost) in kitchen list (see comment line 37)

--- main.js
+++ main.js
@@ -17,7 +17,9 @@
         <div class="order-container">
             <img src="./images/eerste_order.png" alt="Balie met mensen en kassa">
             <button class="kitchenButton">Naar keuken</button>
-            <button class="takeOrder">neem bestelling</button>
+            <div class="order-container">
+              <button class="takeOrder">neem bestelling</button>
+            </div>
         </div>`;
     setTimeout(function () {
       startScreen.classList.add("hidden");
@@ -32,42 +34,94 @@
     const kitchenButton = document.querySelector(".kitchenButton");
     kitchenButton.addEventListener("click", function () {
 
+      //vind nie goe hoe k de newOrder values in de lijst krijg in de keuken
       kitchenScreen.innerHTML = `
       <div class="order-container">
-        <img src="./images/keuken.png" alt="keuken">
-        <button class="counterButton">Naar toog</button>
+      <img src="./images/keuken.png" alt="keuken">
+      <button class="counterButton">Naar toog</button>
+      <div class="order-details">
+        <p>${newOrder.getMeat() || ''}</p>
+        <p>${newOrder.getVeggies() || ''}</p>
+        <p>${newOrder.getDoneness() || ''}</p>
+      </div>
     </div>`;
-    setTimeout(function () {
+
+
+      setTimeout(function () {
         firstOrderScreen.classList.add("hidden");
         kitchenScreen.classList.remove("hidden");
         attachCounterButtonListener()
       }, 500);
 
     });
-    function attachCounterButtonListener(){
-        const counterButton = document.querySelector(".counterButton");
-        counterButton.addEventListener("click",function(){
-            setTimeout(() => {
-                kitchenScreen.classList.add("hidden")
-                firstOrderScreen.classList.remove("hidden")
-            }, 500);
-        });
+    function attachCounterButtonListener() {
+      const counterButton = document.querySelector(".counterButton");
+      counterButton.addEventListener("click", function () {
+        setTimeout(() => {
+          kitchenScreen.classList.add("hidden")
+          firstOrderScreen.classList.remove("hidden")
+        }, 500);
+      });
     }
+
+    const newOrder = new order();
     const orderButton = document.querySelector(".takeOrder");
-    orderButton.addEventListener("click",function(){
-        //nog niet 100% zeker hoe het zit met welke orders we zouden doen maar zou miss een idee zijn voor er en class van te maken
-        //vgm is dat het beste te doen als we het makkelijk willen bijhouden of de orders correct zijn maar running out of time atm
-        //-viho
-    })
+    orderButton.addEventListener("click", function () {
+
+      newOrder.randomizeMeat();
+      newOrder.randomizeVeggies();
+      newOrder.randomizeDoneness();
+
+      orderButton.classList.add("hidden");
+      const secondOrderContainer = document.querySelector(".order-container .order-container:nth-of-type(1)");
+
+      //bug ergens hierrond wanneer je op de orderbutton drukt de .order-container van line 17 groter word
+      secondOrderContainer.innerHTML = `
+      <p>${newOrder.getMeat()}</p>
+      <p>${newOrder.getVeggies()}</p>
+      <p>${newOrder.getDoneness()}</p>`;
+    });
   }
 
 
   class order {
-    constructor() {
-        
+    constructor(meat, veggies, doneness) {
+      this.meat = meat;
+      this.veggies = veggies;
+      this.doneness = doneness;
     }
 
-}
+    getMeat() {
+      return this.meat;
+    }
+
+    getVeggies() {
+      return this.veggies;
+    }
+
+    getDoneness() {
+      return this.doneness;
+    }
+
+    randomizeMeat() {
+      const meats = ["rundvlees", "varkensvlees", "kip", "lam", "kalkoen"];
+      const rnd = Math.floor(Math.random() * meats.length);
+      this.meat = meats[rnd];
+      return this.meat;
+    }
+    randomizeVeggies() {
+      let vegetables = ["wortel", "tomaat", "komkommer", "broccoli", "spinazie"];
+      const rnd = Math.floor(Math.random() * vegetables.length);
+      this.veggies = vegetables[rnd];
+      return this.veggies;
+    }
+    randomizeDoneness() {
+      let donenesses = ["Blue", "Saignant", "A point", "Bien cuit"];
+      const rnd = Math.floor(Math.random() * donenesses.length);
+      this.doneness = donenesses[rnd];
+      return this.doneness;
+    }
+  }
 
 
 })();
